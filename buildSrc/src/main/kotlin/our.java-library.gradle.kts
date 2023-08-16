e42d704fdf6681b1042a8a33d6a276323bcb0296
Caleb Cushing <xenoterracide@gmail.com>
2023-08-16 11:34:47 -0400

    initial

---
+++ buildSrc/src/main/kotlin/our.java-library.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+  `java-library`
+  id("our.bom")
+  id("our.java-compile")
+  id("our.java-test")
+  id("our.checkstyle")
+  id("our.spotbugs")
+  id("our.spotless")
+  id("our.coverage")
+}
