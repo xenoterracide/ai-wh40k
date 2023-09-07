f5bc9c4d6fa377b7e57902bd20a97365a05eb749
Caleb Cushing <xenoterracide@gmail.com>
2023-09-07 14:55:37 -0400

    fix: test for configuration cache

--- buildSrc/src/main/kotlin/our.java-test.gradle.kts
+++ buildSrc/src/main/kotlin/our.java-test.gradle.kts
@@ -1,7 +1,6 @@
 import org.gradle.accessors.dm.LibrariesForLibs
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
-import java.io.FileNotFoundException
 
 plugins {
   `java-library`
@@ -21,11 +20,9 @@ dependencies {
 }
 
 val available = tasks.register("tests available") {
-  val ss = sourceSets;
+  val java: Provider<FileCollection> = sourceSets.test.map { it.java }
   doLast {
-    ss.getByName("test") {
-      if (java.isEmpty) throw FileNotFoundException("no tests found")
-    }
+    if (java.get().isEmpty) throw RuntimeException("no tests found")
   }
 }
 
