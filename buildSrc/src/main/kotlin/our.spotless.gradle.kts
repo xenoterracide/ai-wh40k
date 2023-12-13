e4dd12c746e9e0e59d3ee5e48a950d99ceb7a6fc
Caleb Cushing <caleb.cushing@kroger.com>
2023-12-13 15:23:45 -0500

    add licenses to gradle scripts

--- buildSrc/src/main/kotlin/our.spotless.gradle.kts
+++ buildSrc/src/main/kotlin/our.spotless.gradle.kts
@@ -1,3 +1,18 @@
+/*
+Copyright © 2023 Caleb Cushing.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OFS ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 import org.gradle.kotlin.dsl.support.normaliseLineSeparators
 
 plugins {
@@ -8,7 +23,6 @@ plugins {
 val copyright = "Copyright © \$YEAR Caleb Cushing."
 val license = """
   /*
-
   $copyright
 
   Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,8 +36,7 @@ val license = """
   WITHOUT WARRANTIES OR CONDITIONS OFS ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
-
-  /*
+  */
   """.trimIndent().normaliseLineSeparators()
 
 spotless {
@@ -31,12 +44,9 @@ spotless {
   java {
     licenseHeader(license)
   }
-}
-/*
   kotlinGradle {
-    target("**\/*.gradle.kts")
-    licenseHeader(license, "//")
-    ktfmt()
+    target("**/*.gradle.kts")
+    licenseHeader(license, "^(import|buildscript|plugins)")
+    // ktfmt()
   }
 }
-*/
