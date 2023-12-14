4fe5c4635f4c9b9a62d48d7d0624ea4a53b7b2b5
Caleb Cushing <xenoterracide@gmail.com>
2023-12-13 20:59:51 -0600

    checkstyle and spotless (#61)

--- buildSrc/src/main/kotlin/our.spotless.gradle.kts
+++ buildSrc/src/main/kotlin/our.spotless.gradle.kts
@@ -1,4 +1,20 @@
+/*
+Copyright © 2023 Caleb Cushing.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OFS ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 import com.diffplug.gradle.spotless.SpotlessTask
+import gradle.kotlin.dsl.accessors._3e0fdb0147f372f0cb1483f912b21178.spotless
 import org.gradle.kotlin.dsl.support.normaliseLineSeparators
 
 plugins {
@@ -7,33 +23,40 @@ plugins {
 }
 
 val copyright = "Copyright © \$YEAR Caleb Cushing."
+val license = """
+  /*
+  $copyright
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OFS ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  */
+  """.trimIndent().normaliseLineSeparators()
 
 tasks.withType<SpotlessTask>().configureEach {
-  enabled = !providers.environmentVariable("CI").isPresent
+  enabled = providers.environmentVariable("CI").isPresent
 }
+
 
 spotless {
+  // ratchetFrom("origin/main")
   java {
-    licenseHeader(
-      """
-      /*
-
-      $copyright
-
-      Licensed under the Apache License, Version 2.0 (the "License");
-      you may not use this file except in compliance with the License.
-      You may obtain a copy of the License at
-
-          https://www.apache.org/licenses/LICENSE-2.0
-
-      Unless required by applicable law or agreed to in writing, software
-      distributed under the License is distributed on an "AS IS" BASIS,
-      WITHOUT WARRANTIES OR CONDITIONS OFS ANY KIND, either express or implied.
-      See the License for the specific language governing permissions and
-      limitations under the License.
-
-      */
-      """.trimIndent().normaliseLineSeparators()
-    )
+    licenseHeader(license)
   }
 }
+/*
+kotlinGradle {
+  target("**\*.gradle.kts")
+  licenseHeader(license, "(import|buildscript|plugins|root)")
+  // ktfmt()
+}
+}
+*/
