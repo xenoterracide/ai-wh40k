dec131969aec70c9e48305e90bd1f1848dc00876
Caleb Cushing <xenoterracide@gmail.com>
2023-12-29 22:16:54 -0500

    lock plugins

--- module/game/build.gradle.kts
+++ module/game/build.gradle.kts
@@ -13,7 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OFS ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-buildscript.dependencyLocking.lockAllConfigurations()
+buildscript {
+  dependencyLocking.lockAllConfigurations()
+}
 plugins {
   our.javalibrary
 }
